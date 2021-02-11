520f5042e54d52cfebc86c0d166947aab6c090f4
Alex Tsiofas <alextsiofas@Alexs-MacBook-Air.local>
2021-02-11 11:37:57 -0500

    complete scripts, first draft

---
+++ Download.R
@@ -0,0 +1,25 @@
+#loading the rentrez package
+library(rentrez)
+
+#creating a vector with desired ncbi ids
+ncbi_ids <- c("HQ433692.1","HQ433694.1","HQ433691.1")
+
+#downlaoding the fasta versions of the specified ids in from "nuccore" database on NCBI
+Bburg<-entrez_fetch(db = "nuccore", id = ncbi_ids, rettype = "fasta")
+
+#Splitting the three IDs into separate objects
+Sequences <- strsplit(Bburg, split = "\n\n")
+
+#converting Sequences object to a dataframe
+Sequences<-unlist(Sequences)
+
+#defining the header of each sequence
+header<-gsub("(^>.*sequence)\\n[ATCG].*","\\1",Sequences)
+#defining the sequence
+seq<-gsub("^>.*sequence\\n([ATCG].*)","\\1",Sequences)
+#creating a dataframe with separate name and sequence columns
+Sequences<-data.frame(Name=header,Sequence=seq)
+
+Sequences <- gsub("\\n", "", Sequences)
+
+write.csv(Sequences, "Sequences.csv")
